f2de80372ed0d68886d2ce951dc61163090748b4
Artsem Prashkovich <apr.dev@gmail.com>
2023-09-13 11:40:08 +0200

    Repo cleanup

--- docker/build/README.md
+++
@@ -1,3 +0,0 @@
-# Docker Images for Sitecore Roles
-
-This path contains a build context for Sitecore roles and other supporting images.
--- docker/build/items/CopyItems.ps1
+++
@@ -1,8 +0,0 @@
-Get-ChildItem -Path "C:\temp\src\*\*\items" -Directory| ForEach-Object {
-    $dest = "C:\serialization\src\$($_.FullName.SubString((Get-Item -Path 'C:\temp\src').FullName.Length+1))"
-    $folderPath = $dest.SubString(0,$dest.Length-6)
-    if(!(Test-Path -Path $folderPath -PathType Container)) {
-        New-Item -ItemType Directory -Path $folderPath | Out-Null
-    }
-    Copy-Item $_.FullName -Destination $folderPath -Force -Recurse
-}
